bd47d5d1a895fba339b6e54df9195b1a580f9107
Mina <stanton.mina@gmail.com>
2022-02-02 22:21:50 -0500

    Create Assignment3.py
    Initial commit to create assignment 3 and first issue completed

---
+++ Assignment3.py
@@ -0,0 +1,10 @@
+# Revision 1 2/2/2022
+## Mina Stanton 2/2/2022
+### Initial Commit Issue 1
+
+userNumber = float(input("Please enter a number: "))
+newNumber = (((userNumber + 2) * 3) - 6) / 3
+print(newNumber)
+
+# Revision 1 2/2/2022
+## Mina Stanton
