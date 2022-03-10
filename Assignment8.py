de2e533983147f15bb44006c134b5ec38bb04fd1
Mina <stanton.mina@gmail.com>
2022-03-10 15:38:24 -0500

    Update Assignment8.py

--- Assignment8.py
+++ Assignment8.py
@@ -2,39 +2,78 @@
 ## Mina Stanton 3/7/2022
 ### Initial Commit Issue 1
 
-from tkinter import *
+import tkinter as tk 
 import os 
+import turtle
+import random
+
+t = turtle.Turtle()
 
 # Create display window
-window = Tk()
+window = tk.Tk()
 window.geometry('300x400')
-window.title('Mad Gen')
-Label(window, text='Mad Libs Story Teller', font='Helvectica 20 bold').pack()
-Label(window, text='Click Any One :', font='ariel 15 bold').place(x=40, y=80)
-
-#Madlib one
+window.title('Words, Shapes and more!')
 
 def madlib():
-    person = input('enter a person name: ')
-    color = input('enter a color: ')
-    foods = input('enter a food name: ')
-    adjective = input('enter an adjective name: ')
-    thing = input('enter a thing name: ')
-    place = input('enter a place: ')
-    verb = input('enter a verb: ')
-    adverb = input('enter an adverb: ')
-    food = input('enter food name: ')
-    things = input('enter thing name: ')
-    print('Last night I dreamed I was a ' + adjective + ' unicorn with ' + color + ' with spots that looked like ' + thing + "! I flew into space with " + person + " who is a " + verb + " that was always eating " + food + " we went to " + place + " and had second breakfast which was " + foods + " that tasted like " + adverb + " " + things)
+    person = input('\n\nEnter a person name: ')
+    color = input('Enter a color: ')
+    foods = input('Enter a food name: ')
+    adjective = input('Enter an adjective name: ')
+    thing = input('Enter a thing name: ')
+    place = input('Enter a place: ')
+    verb = input('Enter a verb: ')
+    adverb = input('Enter an adverb: ')
+    food = input('Enter food name: ')
+    things = input('Enter thing name: ')
+    print("\n\nLast night I dreamed I was a " + adjective + " unicorn with " + color + " spots that looked like " + thing + "! I flew into space with " + person + " who is a " + verb + " that was always eating " + food + " we went to " + place + " and had second breakfast which was " + foods + " that tasted like " + adverb + " " + things)
 
-def quit_program():
+# random polygon
+def RandomPolygon():
+    sides =  random.randrange(3,10)
+    sideLength =  random.randrange(20, 45)
+    colors = ["Green", "Red", "Blue", "Yellow", "Black"]
+    polyColor = random.choice(colors)
+    angle = 360/(sides)
+
+    t.begin_fill()
+    t.color(polyColor)
+    for x in range(sides):
+        t.forward(sideLength)
+        t.right(angle)
+    t.end_fill()
+
+ 
+# user input polygon
+def UserPolygon():
+    sides =  int(input("\nEnter the number of sides for the polygon: "))
+    sideLength =  int(input("Enter the side lengths: "))
+    polyColor = input("Enter the color you would like: ")
+    angle = 360/(sides)
+
+    t.forward(100)
+    t.begin_fill()
+    t.color(polyColor)
+    for x in range(sides):
+        t.forward(sideLength)
+        t.right(angle)
+    t.end_fill()
+
+# quit function to exit 
+def Quit_program():
     os._exit(0)
 
 # add and place buttons
-Button(window, text='Mad Lib Go', font='ariel 15', command=madlib, bg= 'ghost white').place(x=70, y=180)
-Button(window, text='QUIT', font=('Helvetica 13 bold'), foreground='Green', background='Yellow', command=quit_program).place(x=80, y=280)
+madLib = tk.Button(window, text='Mad Lib Go', font='ariel 15', command=madlib, bg= 'ghost white')
+randomPolygon = tk.Button(text="Create Random Polygon", font=("Helvetica 13 bold"), foreground="blue", background="ghost white", command=RandomPolygon)
+userPolygon = tk.Button(text="Create Your Own Polygon", font=("Helvetica 13 bold"), foreground="green", background="ghost white", command=UserPolygon)
+quitProgram = tk.Button(window, text='QUIT', font=('Helvetica 13 bold'), foreground='Green', background='Yellow', command=Quit_program)
 
+madLib.pack()
+randomPolygon.pack()
+userPolygon.pack()
+quitProgram.pack()
 
 window.mainloop()
 
 # end of commit 1
+
